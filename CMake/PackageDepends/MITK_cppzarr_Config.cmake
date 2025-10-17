3501dfd72b719417e6ec9bb4559898c68025cb6a
Jonas Cordes <j.cordes@hs-mannheim.de>
2025-10-17 08:23:24 +0200

    Remove cppzarr integration and related files from the project

--- CMake/PackageDepends/MITK_cppzarr_Config.cmake
+++
@@ -1,5 +0,0 @@
-list(APPEND ALL_LIBRARIES ${cppzarr_LIBRARIES})
-list(APPEND ALL_INCLUDE_DIRECTORIES ${cppzarr_INCLUDE_DIRS})
-
-message(STATUS "cppzarr libraries: ${cppzarr_LIBRARIES}")
-message(STATUS "cppzarr include directories: ${cppzarr_INCLUDE_DIRS}")
